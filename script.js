90078138a3a0283e76df16f8bcf3f218071d9db8
Amitoj02 <40449197+Amitoj02@users.noreply.github.com>
2024-01-24 18:07:22 -0500

    Added new project

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-$(document).ready(function () { 
+$(document).ready(function () {
     $(document).click(function () {
-         $('.navbar-collapse').collapse('hide');
+        $('.navbar-collapse').collapse('hide');
     });
+});
+
+var elem = document.querySelector('.projects');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.project-container',
+//   columnWidth: 100
+percentPosition: true
 });
